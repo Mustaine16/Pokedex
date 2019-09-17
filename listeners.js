fbf788bdabefbf05a7357bd94049c60b0b690a35
Eze <ezequieldominguezcp11@gmail.com>
2019-09-17 12:34:20 -0300

    Navbar terminada en funcionalidad, falta el responsive y estilos

--- listeners.js
+++ listeners.js
@@ -5,19 +5,9 @@ search.addEventListener("input", () => {
 
   //Oculta los "1°Gen, 2°Gen, etc"
   if (!(input === "")) {
-
-    nameGenerations.forEach(e => e.style.display = "none")
-    if (window.innerWidth >= 1440) {
-      divEmpty.style.display = "none"
-      divEmpty2.style.display = "none"
-    }
-    
+     nameGenerations.forEach(e => e.style.display = "none")
   } else {
     nameGenerations.forEach(e => e.style.display = "block");
-    if (window.innerWidth >= 1440){
-      divEmpty.style.display = "block"
-      divEmpty2.style.display = "block"
-    }
   }
     
   cards.forEach((card, i) => {
@@ -46,7 +36,6 @@ search.addEventListener("input", () => {
 burger.addEventListener("click", () => {
   let typesBar = document.querySelector(".search-types")
   typesBar.classList.toggle("search-types-active")
-  
 })
 
 document.addEventListener("backButton",() => {
@@ -73,15 +62,26 @@ document.addEventListener("backButton",() => {
 const types = document.querySelectorAll("ul li")
 
 types.forEach((type) => {
-  type.addEventListener('click', () => {
-
+  type.addEventListener('click', (e) => {
+    
     cards.forEach((card, i) => {
+      // console.log(e.target);
 
+      if (e.target.classList[0] === "clearTypes"){
+        card.style.display = "flex"
+        search.value = ""
+        pokemonMissing.classList.remove("pokemon-missing-active")
+        return true
+      }
+      
       let pkType1 = card.classList.item(2);
       let pkType2 = card.classList.item(3) ? card.classList.item(3).slice(0,-1) : false ;
   
       if (pkType2 == type.className || pkType1 == type.className) {
+        search.value = ""
+        nameGenerations.forEach(e => e.style.display = "block");
         card.style.display = "flex"
+        pokemonMissing.classList.remove("pokemon-missing-active")
       }
       else{
         card.style.display = "none";
